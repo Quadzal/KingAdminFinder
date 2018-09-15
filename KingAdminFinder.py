31c6bf6b712fb2072f3e52d0d47a969f309b3c8e
Quadzal <40356538+Quadzal@users.noreply.github.com>
2018-09-15 13:58:36 +0300

    Update KingAdminFinder.py

--- KingAdminFinder.py
+++ KingAdminFinder.py
@@ -1,10 +1,10 @@
 import requests
 
-admin_panel_uzantıları=open("AdminPanelUzantıları.txt",mode="r").readlines()
-site=input("Başında http:// ve Sonunda / Şeklinde Site Adresini Giriniz: ")
+admin_panel_uzantıları = open("AdminPanelUzantıları.txt", mode="r").readlines()
+site = input("Başında http:// ve Sonunda / Şeklinde Site Adresini Giriniz: ")
 
 for i in admin_panel_uzantıları:
-    req=requests.get(site+i.replace("\n",""))
+    req = requests.get(site+i.replace("\n",""))
 
     if req.status_code==200:
         print("Admin Panel Bulundu! ", req.url)
